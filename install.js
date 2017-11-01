d047cd626de6351843fc388bea29b65d4e5881b4
阙中友 <837300621@qq.com>
2017-11-01 16:09:33 +0800

    修改

--- install.js
+++ install.js
@@ -31,8 +31,31 @@ exports.register = function (commander) {
             if (setting.useYeoman) {
 
             } else {
-                let Generater =require("./lib/generater");
-                new Generater(setting);
+                let Generater = require("./lib/generater");
+                var co = require("co");
+                var gulp = require("gulp");
+
+                var generater = new Generater(setting);
+
+                co(function *() {
+                    //执行安装
+                    if (yield Generater._install.bind(generater)()) {
+                        //初始化,执行模块
+                        gulp.task("init", generater.init.bind(generater));
+
+                        // 提问
+                        gulp.task("props", ['init'], function(next){
+                            Generater.base.prompting.bind(generater)(next);
+                        });
+
+                        //构建
+                        gulp.task("create", ['init', "props"],function (next) {
+                            Generater._create.bind(generater)(next);
+                        });
+
+                        gulp.start(['init',"props","create"]);
+                    }
+                })
             }
         })
 }
